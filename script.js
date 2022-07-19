6f491bff9553f5c0753facfb961fd1794c4d28bc
erayceylan <erayceylan@rocketmail.com>
2022-07-19 12:10:39 +0300

    Add resolution option

--- script.js
+++ script.js
@@ -1,22 +1,88 @@
-console.log("hello me again");
-
+/* Defining canvas */
 const canvas = document.querySelector(".canvas");
-console.log(canvas);
 
-const pixels = [];
+/* Defining control options */
+const resolution = document.querySelectorAll("input[name='resolution']");
+resolution.forEach(option => option.addEventListener('change',updateCanvas));
 
-for (let i = 0; i < 256; i++) {
-    pixels[i] = document.createElement("div");
-    pixels[i].style.width = "16px"
-    pixels[i].style.height = "16px"
-};
+initializeCanvas()
 
-console.log(pixels);
+function updateCanvas() {
+    
+    removePixels()
+    initializeCanvas()
 
-pixels.forEach( pixel => {canvas.appendChild(pixel);});
-pixels.forEach( pixel => pixel.addEventListener('mouseover',mouseOver));
-pixels.forEach( pixel => pixel.addEventListener('mouseout',mouseOut));
+}
 
+function initializeCanvas() {
+
+    /*
+    this function will:
+    -uptade the pixels in the canvas
+    */
+
+    let pixels = createPixels();
+    addPixels(pixels);
+    addDrawing(pixels);
+}
+
+function addPixels(pixels) {
+
+    /*
+    input-argument: array
+    this function will:
+    -add pixels in the list to the canvas
+    */
+
+    pixels.forEach( pixel => {canvas.appendChild(pixel);});
+}
+
+function removePixels() {
+
+    /*
+    input-argument: array
+    this function will:
+    -select all the pixels in the canvas
+    -remove all the pixels from the canvas
+    */
+
+    let pixels = document.querySelectorAll('.canvas div')
+    pixels.forEach( pixel => {canvas.removeChild(pixel);});
+}
+
+function createPixels() {
+
+    /* 
+    this function will:
+    -create an empty pixel list
+    -add pixels according to user's selection of resolution
+    -return list of pixels
+    */
+
+    let pixels = [];
+    let pixelSize = +document.querySelector('input[name="resolution"]:checked').value;
+    let resolution = pixelSize**2
+
+    for (let i = 0; i < resolution; i++) {
+        pixels[i] = document.createElement("div");
+        pixels[i].style.width = `${512/pixelSize}px`;
+        pixels[i].style.height = `${512/pixelSize}px`;
+    }
+
+    return pixels
+}
+
+function addDrawing(pixels) {
+
+    /*
+    this function will:
+    -add mouseover event listener to the canvas pixels
+    -add mouseout event listener to the canvas pizels
+    */
+
+    pixels.forEach( pixel => pixel.addEventListener('mouseover',mouseOver));
+    pixels.forEach( pixel => pixel.addEventListener('mouseout',mouseOut));
+}
 
 function mouseOver() {
     this.style.transitionDuration ="0.05s"
